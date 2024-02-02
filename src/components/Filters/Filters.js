b427614d05a5cfa0ab1e817ecde774fae3d41e3c
quockhanh04 <22520637@gm.uit.edu.vn>
2024-02-02 23:04:52 +0700

    css for Filters

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,18 +1,79 @@
 import { v4 as id } from "uuid";
-import "./Filters.scss";
+import { useState } from "react";
 
-const STATUS = [{ id: id() }];
+import "./Filters.scss";
+const STATUS = [
+  { id: id(), value: "All" },
+  { id: id(), value: "Completed" },
+  { id: id(), value: "Todo" },
+];
+
+const PRIORITY = [
+  { id: id(), value: "All" },
+  { id: id(), value: "High" },
+  { id: id(), value: "Medium" },
+  { id: id(), value: "Low" },
+];
 
 function Filters() {
+  const [priority, setPriority] = useState("All");
+  const handlePriority = (priority) => {
+    setPriority(priority);
+  };
   return (
-    <div className="filter-wrapper">
+    <div className="filters-wrapper">
       <div className="filters-search">
         <h4 className="heading-title">Search</h4>
-        <input type="text" placeholder="Search..." />
-        <button></button>
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <button>
+            <svg
+              viewBox="64 64 896 896"
+              focusable="false"
+              data-icon="search"
+              width="1em"
+              height="1em"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
+            </svg>
+          </button>
+        </div>
       </div>
       <div className="filters-status">
         <h4 className="heading-title">Filter by status</h4>
+        <div className="status">
+          {STATUS.map((item) => {
+            return (
+              <div className="status-item">
+                <input key={item.id} id={item.id} type="radio" name="status" />
+                <label htmlFor={item.id}>
+                  <span>{item.value}</span>
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="filters-priority">
+        <h4 className="heading-title">Filter by priority</h4>
+        <div className="priority">
+          <select
+            name=""
+            id=""
+            value={priority}
+            onChange={(e) => handlePriority(e.target.value)}
+          >
+            {PRIORITY.map((item) => {
+              return (
+                <option key={item.id} value={item.value}>
+                  {item.value}
+                </option>
+              );
+            })}
+          </select>
+        </div>
       </div>
     </div>
   );
